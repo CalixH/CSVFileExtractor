b7e33b84ca53411f8e02cde2d991670feeb98594
Victor <70664921+victorr-su@users.noreply.github.com>
2022-05-13 12:51:18 -0400

    Update CSVfileextractor.py

--- CSVfileextractor.py
+++ CSVfileextractor.py
@@ -7,4 +7,10 @@ root = tk.Tk()
 root.withdraw()
 
 # Main file path where all digital agent folders are located ( in this case, 3rd party links)
-Absfile_path = filedialog.askopenfilename()
+Mainfile_path = filedialog.askopenfilename()
+
+# File path for the 4 subfiles in the 
+file_path1 = Mainfile_path + "/DigitalAgent.ca"
+file_path2 = Mainfile_path + "/DigitalAgent.com"
+file_path3 = Mainfile_path + "/DigitalAgent.net"
+file_path4 = Mainfile_path + "/DigitalAgent.org"
