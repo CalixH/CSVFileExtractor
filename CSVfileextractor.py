8892e9b5c485db164e6ca3c85188b0206324986c
CalixH <c295huan@uwaterloo.ca>
2022-05-16 10:08:20 -0400

    haha

--- CSVfileextractor.py
+++ CSVfileextractor.py
@@ -2,6 +2,7 @@ import pandas as pd #panda is an excel manager for python
 import glob #glob is a filepath manager
 from tkinter import Tk, filedialog # tkinter is for file selection popup dialogues
 from openpyxl import load_workbook #excel read/writer
+import os
 
 pd.set_option('display.max_rows', None)
 
@@ -22,27 +23,34 @@ file_path4 = Mainfile_path + "/DigitalAgent.org"
 
 # Array of all file paths
 file_paths = [file_path1,file_path2,file_path3,file_path4]
-file_names = ["BlogAudit.sql.csv", "ComplianceQueueAudit.sql.csv", "ContentAudit.sql.csv", "DirectoryAudit.sql.csv", "DisclosureAudit.sql.csv", "EventAudit.sql.csv", "FileAudit.sql.csv", "OfficeVersionAudit.sql.csv", "OptionsAudit.sql.csv", "OrganizationalGroupAudit.sql.csv", "PageAudit.sql.csv", "ProfileAudit.sql.csv", "RepositoryAudit.sql.csv", "ResourceAudit.sql.csv", "SettingsAudit.sql.csv", "SubmissionFormVersionAudit.sql.csv", "TenantAudit.sql.csv", "UserAudit.sql.csv", "WebsiteAudit.sql.csv"]
+file_names = ["BlogAudit", "ComplianceQueueAudit", "ContentAudit", "DirectoryAudit", "DisclosureAudit", "EventAudit", "FileAudit", "OfficeVersionAudit", "OptionsAudit", "OrganizationalGroupAudit", "PageAudit", "ProfileAudit", "RepositoryAudit", "ResourceAudit", "SettingsAudit", "SubmissionFormVersionAudit", "TenantAudit", "UserAudit", "WebsiteAudit"]
+file_tail = ".sql.csv"
 
 # Getting all the files from their respective folders and putting it into one variable. There are 
 # 20 different combined files, so we will use 20 variables to hold all the files from each respective folder
 
 #blogAudit files
+counter = 0
 
-all_file_BlogAudit = []
+if os.path.isdir(Mainfile_path + "/Digital Agent Combined") == False:
+    os.mkdir(Mainfile_path + "/Digital Agent Combined")
 
-for num,file in enumerate(file_paths):
-    all_file_BlogAudit += glob.glob(file_paths[num] + "/" + file_names[0])
+for name in file_names:
+    all_file_BlogAudit = []
 
-BlogAuditList = []
+    for num,file in enumerate(file_paths):
+        all_file_BlogAudit += glob.glob(file_paths[num] + "/" + file_names[counter] + file_tail)
 
-for filename in all_file_BlogAudit:
-    df_BlogAudit = pd.read_csv(filename, index_col = None, header = 0 )
-    BlogAuditList.append(df_BlogAudit)
+    BlogAuditList = []
 
-df_BlogAudit = pd.concat(BlogAuditList, axis = 0, ignore_index = True)
+    for filename in all_file_BlogAudit:
+        df_BlogAudit = pd.read_csv(filename, index_col = None, header = 0 )
+        BlogAuditList.append(df_BlogAudit)
 
-df_BlogAudit.to_excel(Mainfile_path + "/test1.xlsx", index=False)
+    df_BlogAudit = pd.concat(BlogAuditList, axis = 0, ignore_index = True)
+
+    df_BlogAudit.to_excel(Mainfile_path + "/Digital Agent Combined/" + name + "Combined.xlsx", index=False)
+    counter += 1
 #####################################################################################
 
 
